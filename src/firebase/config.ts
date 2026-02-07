9f2ba859201a201bb313d3aa15ffa7604b6ea828
jeyasanthoshinn-glitch <jeyasanthoshinn@gmail.com>
2026-02-07 13:16:35 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,29 +3,37 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 //THIS IS THE MAIN DB
-  const firebaseConfig = {
-    apiKey: "AIzaSyCmBdthR6ikDTR1VvmPkhpaQ9pYQjQw-Wg",
-    authDomain: "jeya-santhosh-inn-b02dd.firebaseapp.com",
-    projectId: "jeya-santhosh-inn-b02dd",
-    storageBucket: "jeya-santhosh-inn-b02dd.firebasestorage.app",
-    messagingSenderId: "330001746216",
-    appId: "1:330001746216:web:9b384a5851afaf0cae681d",
-    measurementId: "G-WCV81FB8HH"
-  };
-
+/*const firebaseConfig = {
+  apiKey: "AIzaSyD57ySp5pnHPxX_3UaveoC62lZHFOUrT-Y",
+  authDomain: "bossinn-44651.firebaseapp.com",
+  projectId: "bossinn-44651",
+  storageBucket: "bossinn-44651.firebasestorage.app",
+  messagingSenderId: "510548886376",
+  appId: "1:510548886376:web:2a4d9f0e8724090cc7ccdd",
+  measurementId: "G-596DPL3V12"
+};*/
 
 
 //TESTING
 
 // const firebaseConfig = {
-//   apiKey: "AIzaSyAIxrA5uo64FQOq_RP1GnG8sNhhXJBaX2E",
-//   authDomain: "boss-test-3d2f8.firebaseapp.com",
-//   projectId: "boss-test-3d2f8",
-//   storageBucket: "boss-test-3d2f8.firebasestorage.app",
-//   messagingSenderId: "820395646920",
-//   appId: "1:820395646920:web:38b8361365c00c74ce2afd",
-//   measurementId: "G-HHBR54NKFM"
+//   apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
+//   authDomain: "bossinn-7fbb9.firebaseapp.com",
+//   projectId: "bossinn-7fbb9",
+//   storageBucket: "bossinn-7fbb9.firebasestorage.app",
+//   messagingSenderId: "138358328390",
+//   appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
+//   measurementId: "G-8KRR5T7YJQ"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyAIxrA5uo64FQOq_RP1GnG8sNhhXJBaX2E",
+  authDomain: "boss-test-3d2f8.firebaseapp.com",
+  projectId: "boss-test-3d2f8",
+  storageBucket: "boss-test-3d2f8.firebasestorage.app",
+  messagingSenderId: "820395646920",
+  appId: "1:820395646920:web:38b8361365c00c74ce2afd",
+  measurementId: "G-HHBR54NKFM"
+};
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
